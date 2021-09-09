04852f8b0c7cc5f4aaf5d36f0a7a3ec6e33b603f
Horlawale-ui <53876892+Horlawale-ui@users.noreply.github.com>
2021-09-09 06:00:17 +0100

    Update

--- src/app/modules/dashboard/sendfund/sendfund.component.ts
+++ src/app/modules/dashboard/sendfund/sendfund.component.ts
@@ -1,4 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Profile } from 'src/app/interfaces/profile';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-sendfund',
@@ -11,25 +13,64 @@ export class SendfundComponent implements AfterViewInit {
   @ViewChild('deposit') deposit: ElementRef;
 
   depositModal: any;
+  planPreview: any;
+  planPreview2: any;
+  planPreview3: any;
+  option: string = "Null";
+  amount: any;
+  paymentMethod: any;
+  userId: any;
+  users: any;
+  user: any;
 
-  constructor(private renderer: Renderer2) { }
+  constructor(private renderer: Renderer2, private profileService: ProfileService) { }
 
   ngAfterViewInit(): void {
+
+    // Get single User Informations
+    this.userId = JSON.parse(localStorage.getItem('id') || '{}')
+    this.users = this.profileService.getSingleUser(this.userId).subscribe((res: any) => {
+      res.forEach((r: any) => {
+        let item = r.payload.doc.data() as Profile
+        this.user = item
+      });
+    })
+
     // Click Outside to close element
-    this.renderer.listen('window', 'click', (e: Event) => {
-      let x = !this.investBtn.nativeElement.contains(e.target)
-      let y = !this.deposit.nativeElement.contains(e.target)
-      if (x && y) {
-        this.depositModal = false;
-      }
-    });
+    // this.renderer.listen('window', 'click', (e: Event) => {
+    //   let x = !this.investBtn.nativeElement.contains(e.target)
+    //   let y = !this.deposit.nativeElement.contains(e.target)
+    //   if (x && y) {
+    //     this.depositModal = false;
+    //   }
+    // });
   }
 
-
-  // Open Deposit Modal
+  // Submit form
   openDepositModal() {
+    this.amount = document.getElementById("amount");
+    this.paymentMethod = document.getElementById("paymentMethod");
+
+    // Open Deposit Modal
     this.depositModal = !this.depositModal
-    console.log("Yes!!!")
+
+    if (this.paymentMethod.value === 'Btc') {
+      this.option = "Btc"
+    } else if (this.paymentMethod.value === 'Etherum') {
+      this.option = "Etherum"
+    }
+
+  }
+
+  // Open Plan Preview
+  openPlanPreview() {
+    this.planPreview = !this.planPreview
+  }
+  openPlanPreview2() {
+    this.planPreview2 = !this.planPreview2
+  }
+  openPlanPreview3() {
+    this.planPreview3 = !this.planPreview3
   }
 
 }
