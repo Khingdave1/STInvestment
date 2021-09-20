5a051862cc5b27dd7d469327aef1853a70073d28
Aremu David <55801713+Khingdave1@users.noreply.github.com>
2021-09-20 23:11:54 +0100

    Update about.component.ts

--- src/app/modules/about/about.component.ts
+++ src/app/modules/about/about.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('certBtn') certBtn: ElementRef;
+  @ViewChild('cert') cert: ElementRef;
 
-  ngOnInit(): void {
+  certModal: any;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngAfterViewInit(): void {
+    // Click Outside to close element
+    this.renderer.listen('window', 'click', (e: Event) => {
+      let x = !this.certBtn.nativeElement.contains(e.target)
+      let y = !this.cert.nativeElement.contains(e.target)
+      if (x && y) {
+        this.certModal = false;
+      }
+    });
+  }
+
+  // Open Cert Modal
+  openCertModal() {
+    this.certModal = !this.certModal
   }
 
 }
