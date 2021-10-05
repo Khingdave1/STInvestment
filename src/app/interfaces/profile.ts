7fd627d910e1a7e9f3373c1fbbf06f91e8bde7e4
Aremu David <55801713+Khingdave1@users.noreply.github.com>
2021-10-05 13:29:53 +0100

    Update profile.ts

--- src/app/interfaces/profile.ts
+++ src/app/interfaces/profile.ts
@@ -1,8 +1,12 @@
 export interface Profile {
+  id?: any;
   emailAddress: string;
   fullName: string;
   userName: string;
   telNumber: number;
   imageUrl: string;
   uid: string;
+  currentBal: number;
+  totalDepo: number;
+  totalWith: number;
 }
