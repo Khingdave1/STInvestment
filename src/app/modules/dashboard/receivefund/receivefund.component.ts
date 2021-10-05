bef31ff667c346ce8dacd15f5687788b3b9845b2
Aremu David <55801713+Khingdave1@users.noreply.github.com>
2021-10-05 13:28:16 +0100

    Update receivefund.component.ts

--- src/app/modules/dashboard/receivefund/receivefund.component.ts
+++ src/app/modules/dashboard/receivefund/receivefund.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Profile } from 'src/app/interfaces/profile';
+import { ProfileService } from 'src/app/services/profile.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-receivefund',
@@ -7,14 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceivefundComponent implements OnInit {
 
-  constructor() { }
+  userId: any;
+  users: any;
+  user: any;
+  successMessage: boolean = false;
+  currentTotalWith: any;
+  constructor(private formBuilder: FormBuilder, private profileService: ProfileService, private router: Router) { }
 
   ngOnInit(): void {
+
+    // Get single User Informations
+    this.userId = JSON.parse(localStorage.getItem('id') || '{}')
+    this.users = this.profileService.getSingleUser(this.userId).subscribe((res: any) => {
+      res.forEach((r: any) => {
+        let item = r.payload.doc.data() as Profile
+        item.id = r.payload.doc.id
+        this.user = item
+      });
+    })
   }
 
-    // Deposit
-    withdrawlbtn() {
-      // Reload page
-      window.location.reload()
+
+  // Withdraw Form
+  withdrawForm: FormGroup = this.formBuilder.group({
+    amount: ['', { validators: [Validators.required], updateOn: "change" }],
+  });
+
+  // Edit Total Withdraw
+  onEdit(data: any) {
+    let payload = {
+      recentAmount: this.withdrawForm.value.amount,
+      totalWith: data.totalWith + this.withdrawForm.value.amount
     }
+
+    this.profileService.updateUser(data.id, payload).then(res => {
+      this.successMessage = true
+
+      // Navigate to Dashboard
+      this.router.navigate(['dashboard'])
+    }).catch(err => {
+      console.log(err)
+    })
+  }
 }
