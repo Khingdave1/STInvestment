14cc48496de061921c9a1d0b9883257798eb05c4
Aremu David <55801713+Khingdave1@users.noreply.github.com>
2021-10-10 17:25:48 +0100

    Update profile.service.ts

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -15,7 +15,7 @@ export class ProfileService {
   // CREATE
   addUser(payload: any) {
     return new Promise<any>((resolve, reject) => {
-      this.collectionPath.add(payload).then(res => resolve(res), err => reject(err))
+      this.db.doc(this.collectionName + '/' + payload.userName).set(payload).then(res => resolve(res), err => reject(err))
     })
   }
 
